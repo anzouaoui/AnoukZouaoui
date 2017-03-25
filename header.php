bf7ef309a93d7e77c2bb38cc6978c8fde242c943
Anouk ZOUAOUI <anzouaoui78@yahoo.fr>
2017-03-25 18:11:01 +0100

    Mise en place du header

---
+++ header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Portfolio d'Anouk ZOUAOUI développeur web et mobile junior </title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/banniere.css">
+        <link rel="stylesheet" href="css/menu.css">
+    </head>
+    <body>
+      <header>
+        <!-- Debut du menu-->
+        <div class="menu">
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 noPadd" style="height: 100%">
+                <div class="navbar-header">
+                  <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                </div>
+                <div class="navbar-nav nav navbar-collapse">
+                  <ul>
+                    <li class="menuItem active">
+                      <a href="#wrapper">Accueil</a>
+                    </li>
+                    <li class="menuItem">
+                      <a href="#aPropos">A propos</a>
+                    </li>
+                    <li class="menuItem">
+                      <a href="#formation">Formation</a>
+                    </li>
+                    <li class="menuItem">
+                      <a href="#competences">Compétences</a>
+                    </li>
+                    <li class="menuItem">
+                      <a href="#experiences">Experiences</a>
+                    </li>
+                    <li class="menuItem">
+                      <a href="#realisations">Réalisations</a>
+                    </li>
+                    <li class="menuItem">
+                      <a href="#contact">Contact</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+          <!-- Debut du Wrapper-->
+            <div class="wrapper" id="wrapper">
+                <!-- Bannièere-->
+                <div class="banniere">
+                  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 noPadd" style="height: 100%">
+                    <div class="bannierePartie">
+                      <img src="Images/banniere.jpg" alt="" class="imageBanniere">
+                    </div>
+                  </div>
+                </div>
+                <div class="textBanniere container">
+                  <h1>Bienvenue sur mon portfolio</h1>
+                  <h2>Anouk Zouaoui</h2>
+                </div>
+            </div>
+        </header>
